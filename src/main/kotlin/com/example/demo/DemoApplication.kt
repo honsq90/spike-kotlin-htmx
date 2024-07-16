5471a46cb12a44e34cd4ef635b80c42a8c3b359e
Shuqian Hon <honsq90@gmail.com>
2024-07-16 16:28:30 +1000

    Initial

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,26 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+
+
+
+@Controller
+class TestController {
+	@GetMapping
+	fun renderDashboard(): String {
+		return createHtml().html {
+			
+		}
+	}
+}
