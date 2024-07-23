90addaa838e11600f30325ab31d7fdacb75178d6
Shuqian Hon <honsq90@gmail.com>
2024-07-23 23:27:12 +1000

    Actually paginate data using UI, with pagination JUnit tests

--- src/main/kotlin/com/example/demo/ExamplePaginatedTableController.kt
+++ src/main/kotlin/com/example/demo/ExamplePaginatedTableController.kt
@@ -4,6 +4,7 @@ import io.github.serpro69.kfaker.faker
 import kotlinx.html.*
 import kotlinx.html.stream.createHTML
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -18,7 +19,7 @@ class ExamplePaginatedTableController {
 
     private val faker = faker {}
     private val list: List<TableDataRow> by lazy {
-        List(50) {
+        List(75) {
             TableDataRow(
                 id = it,
                 name = faker.name.firstName(),
@@ -31,80 +32,126 @@ class ExamplePaginatedTableController {
     @GetMapping("/example/paginated-table")
     fun paginatedTable(): String {
 
-        val rows = list.take(10)
-
         return createHTML()
             .div(classes = "text-center") {
                 h2 { +"""Paginated table""" }
                 div("container mt-5") {
-                    div("row mb-3 justify-content-end") {
-                        div("col-auto") {
-                            input(classes = "form-control") {
-                                type = InputType.text
-                                placeholder = "Filter..."
-                            }
+                    id = "paginated-table-container"
+                    attributes["hx-get"] = "/example/paginated-table/search"
+                    attributes["hx-swap"] = "outerHTML"
+                    attributes["hx-trigger"] = "load"
+
+                }
+            }
+    }
+
+
+    @GetMapping("/example/paginated-table/search")
+    fun tableSearch(
+        @RequestParam(required = false) page: Int = 1,
+        @RequestParam(required = false) pageSize: Int = 10,
+    ): String {
+        // Calculate pagination offsets
+        val startIndex = (page - 1) * pageSize
+        val endIndex = minOf(startIndex + pageSize, list.size)
+
+        // Extract items for the current page
+        val rows = list.subList(startIndex, endIndex)
+
+        val totalPages = list.size / pageSize
+
+        return createHTML()
+            .div("container mt-5") {
+                id = "paginated-table-container"
+                div("row mb-3 justify-content-end") {
+                    div("col-auto") {
+                        input(classes = "form-control") {
+                            type = InputType.text
+                            placeholder = "Filter..."
                         }
                     }
-                    table("table table-striped") {
-                        thead {
+                }
+                table("table table-striped") {
+                    thead {
+                        tr {
+                            th { +"""#""" }
+                            th { +"""Name""" }
+                            th { +"""Role""" }
+                            th { +"""Email""" }
+                        }
+                    }
+                    tbody {
+                        rows.forEach { row ->
                             tr {
-                                th { +"""#""" }
-                                th { +"""Name""" }
-                                th { +"""Role""" }
-                                th { +"""Email""" }
-                            }
-                        }
-                        tbody {
-                            rows.forEach { row ->
-                                tr {
-                                    td { +"${row.id}" }
-                                    td { +row.name }
-                                    td { +"""Developer""" }
-                                    td { +row.email }
-                                }
-                            }
-                        }
-                    }
-                    nav {
-                        attributes["aria-label"] = "Page navigation"
-                        ul("pagination justify-content-end") {
-                            li("page-item disabled") {
-                                a(classes = "page-link") {
-                                    href = "#"
-                                    tabIndex = "-1"
-                                    attributes["aria-disabled"] = "true"
-                                    +"""Previous"""
-                                }
-                            }
-                            li("page-item active") {
-                                attributes["aria-current"] = "page"
-                                a(classes = "page-link") {
-                                    href = "#"
-                                    +"""1"""
-                                    span("visually-hidden") { +"""(current)""" }
-                                }
-                            }
-                            li("page-item") {
-                                a(classes = "page-link") {
-                                    href = "#"
-                                    +"""2"""
-                                }
-                            }
-                            li("page-item") {
-                                a(classes = "page-link") {
-                                    href = "#"
-                                    +"""3"""
-                                }
-                            }
-                            li("page-item") {
-                                a(classes = "page-link") {
-                                    href = "#"
-                                    +"""Next"""
-                                }
+                                td { +"${row.id}" }
+                                td { +row.name }
+                                td { +"""Developer""" }
+                                td { +row.email }
                             }
                         }
                     }
                 }
+                nav {
+                    attributes["aria-label"] = "Page navigation"
+                    ul("pagination justify-content-end") {
+                        val previousClasses = if (page == 1) {
+                            "page-item disabled"
+                        } else {
+                            "page-item"
+                        }
+                        li(classes = previousClasses) {
+                            id="pagination-prev"
+                            a(classes = "page-link") {
+                                attributes["hx-get"] = "/example/paginated-table/search?page=${page - 1}"
+                                attributes["hx-target"] = "#paginated-table-container"
+                                tabIndex = "-1"
+                                if (page == 1) {
+                                    attributes["aria-disabled"] = "true"
+                                }
+                                +"""Previous"""
+                            }
+                        }
+                        (0..totalPages).forEach {
+                            val pageIndex = it + 1
+                            val isCurrent = pageIndex == page
+                            val listClasses = if (isCurrent) {
+                                "page-item active"
+                            } else {
+                                "page-item"
+                            }
+                            li(classes = listClasses) {
+                                if (isCurrent) {
+                                    attributes["aria-current"] = "page"
+                                }
+                                a(classes = "page-link") {
+                                    attributes["hx-get"] = "/example/paginated-table/search?page=${pageIndex}"
+                                    attributes["hx-target"] = "#paginated-table-container"
+                                    +"""$pageIndex"""
+                                    if (isCurrent) {
+                                        span("visually-hidden") { +"""(current)""" }
+                                    }
+                                }
+                            }
+                        }
+                        val nextClasses = if (page > totalPages) {
+                            "page-item disabled"
+                        } else {
+                            "page-item"
+                        }
+                        li(classes = nextClasses) {
+                            id="pagination-next"
+                            a(classes = "page-link") {
+                                attributes["hx-get"] = "/example/paginated-table/search?page=${page + 1}"
+                                attributes["hx-target"] = "#paginated-table-container"
+                                if (page > totalPages) {
+                                    attributes["aria-disabled"] = "true"
+                                }
+                                +"""Next"""
+                            }
+                        }
+                    }
+                }
+
             }
     }
 }
--- src/test/kotlin/com/example/demo/ExamplePaginatedTableControllerTest.kt
+++ src/test/kotlin/com/example/demo/ExamplePaginatedTableControllerTest.kt
@@ -2,12 +2,13 @@ package com.example.demo
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.junit.jupiter.MockitoExtension
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
 
 @ExtendWith(MockitoExtension::class)
 class ExamplePaginatedTableControllerTest {
@@ -21,8 +22,38 @@ class ExamplePaginatedTableControllerTest {
 
         val doc: Document = Jsoup.parse(html)
 
-        val result = doc.getElementsByTag("table")
+        val result = doc.getElementById("paginated-table-container")
 
-        kotlin.test.assertEquals(1, result.size)
+        assertNotNull(result)
+    }
+
+    @Test
+    fun `tableSearch - should render first page`() {
+        val html = controller.tableSearch()
+
+        val doc: Document = Jsoup.parse(html)
+
+        val result = doc.select("tbody tr")
+        assertEquals(10, result.size)
+
+        val previousButton = doc.getElementById("pagination-prev")
+        val nextButton = doc.getElementById("pagination-next")
+        assertEquals("page-item disabled", previousButton.className())
+        assertEquals("page-item", nextButton.className())
+    }
+
+    @Test
+    fun `tableSearch - should render last page`() {
+        val html = controller.tableSearch(page = 8)
+
+        val doc: Document = Jsoup.parse(html)
+
+        val result = doc.select("tbody tr")
+        assertEquals(5, result.size)
+
+        val previousButton = doc.getElementById("pagination-prev")
+        val nextButton = doc.getElementById("pagination-next")
+        assertEquals("page-item", previousButton.className())
+        assertEquals("page-item disabled", nextButton.className())
     }
 }
